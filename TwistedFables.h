7cf69519e9a4152481ffbf92a6f2bfab2ca19693
LeoTengAMao <LeoTeng995@gmail.com>
2025-04-17 13:58:56 +0800

    Add files via upload

---
+++ TwistedFables.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#define Ult_threshold Ultt
+#define Maxpower 25
+
+typedef struct Character{
+	int8_t Maxhp;
+	int8_t Maxarmor;
+	int8_t Ult_threshold;
+	int8_t token;
+}character
+
+typedef struct Player{
+	int8_t Maxhp;
+	int8_t hp;
+	int8_t Maxarmor;
+	int8_t armor;
+	int8_t Ult_threshold;
+	int8_t power;
+	int8_t hands;
+	int8_t token;
+}player
+
+typedef struct Card{
+	int8_t cost;
+	int8_t level;
+	int8_t range; //for attack 0 means its doesn't need range
+	int8_t type; // 0)attack 1)defend 2)movement 3)skills 4)Ult 5)passive
+	int8_t power_generate;
+	int8_t value; // for example value = 1 on attack => deal 1 damage
+	int8_t require_basic_card; // 0 means do not requires
+	int16_t cardcode; // for skill card 
+}card 
+
+	
