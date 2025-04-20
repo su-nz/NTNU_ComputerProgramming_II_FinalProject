a203027a4091c0de52292d5f6162cf94a9991cce
LeoTengAMao <LeoTeng995@gmail.com>
2025-04-20 12:28:48 +0800

    Update TwistedFables.c

--- TwistedFables.c
+++ TwistedFables.c
@@ -1,6 +1,7 @@
 #include "TwistedFables.h"
 
 int main(){
+	//int8_t char_choosen[4]= {-1,-1,-1,-1};
 	srand( time(NULL) );
 	//mode=-1
 	printf("請你選擇你想要遊玩的模式：(1.)1對1 (2.)2對2 （請輸入數字來做決定\n");
@@ -17,46 +18,170 @@ int main(){
 		printf("請你重新輸入\n");
 		printf("是否要啟動遺跡模式？：(1.)是 (2.)否 （請輸入數字來做決定\n");
 	} 
+	//BotOn = -1
+	printf("是否要啟用機器人？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+	while (scanf("%hhd",&BotOn)!=1 || !(BotOn == 1 || BotOn == 2 )){
+		getchar();
+		printf("請你重新輸入\n");
+		printf("是否要啟用機器人？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+	} 
 	//player1_char = -1
-	printf("====================\n");
+	printf("\n====================\n");
 	printf("0)小紅帽\n");
 	printf("1)白雪公主\n");
 	printf("2)睡美人\n");
+	printf("3)愛麗絲\n");
+	printf("4)花木蘭\n");
+	printf("5)輝夜姬\n");
+	printf("6)美人魚\n");
+	printf("7)火柴女孩\n");
+	printf("8)逃樂絲\n");
+	printf("9)山魯佐德\n");
 	printf("10)隨機\n");
 	printf("====================\n");
 	printf("玩家一請選擇你要遊玩的角色：");
 	while (scanf("%hhd",&player1_char)!=1 || !(player1_char >= 0 && player1_char <= 10 )){
 		getchar();
-		printf("請你重新輸入\n");
+		printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 		printf("====================\n");
 		printf("0)小紅帽\n");
 		printf("1)白雪公主\n");
 		printf("2)睡美人\n");
+		printf("3)愛麗絲\n");
+		printf("4)花木蘭\n");
+		printf("5)輝夜姬\n");
+		printf("6)美人魚\n");
+		printf("7)火柴女孩\n");
+		printf("8)逃樂絲\n");
+		printf("9)山魯佐德\n");
 		printf("10)隨機\n");
 		printf("====================\n");
 		printf("玩家一請選擇你要遊玩的角色：");
 		
 	} 
-	if(mode == 2){
+	printf("\n====================\n");
+	printf("0)小紅帽\n");
+	printf("1)白雪公主\n");
+	printf("2)睡美人\n");
+	printf("3)愛麗絲\n");
+	printf("4)花木蘭\n");
+	printf("5)輝夜姬\n");
+	printf("6)美人魚\n");
+	printf("7)火柴女孩\n");
+	printf("8)逃樂絲\n");
+	printf("9)山魯佐德\n");
+	printf("10)隨機\n");
+	printf("====================\n");
+	if(BotOn==1){
+		printf("請選擇電腦要遊玩的角色：");
+	}else{
+		printf("玩家二請選擇你要遊玩的角色：");
+	}
+	while (scanf("%hhd",&player2_char)!=1 || !(player2_char >= 0 && player2_char <= 10 )||(player2_char == player1_char)){
+		getchar();
+		printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 		printf("====================\n");
 		printf("0)小紅帽\n");
 		printf("1)白雪公主\n");
 		printf("2)睡美人\n");
+		printf("3)愛麗絲\n");
+		printf("4)花木蘭\n");
+		printf("5)輝夜姬\n");
+		printf("6)美人魚\n");
+		printf("7)火柴女孩\n");
+		printf("8)逃樂絲\n");
+		printf("9)山魯佐德\n");
 		printf("10)隨機\n");
 		printf("====================\n");
-		printf("玩家二請選擇你要遊玩的角色：");
-		while (scanf("%hhd",&player2_char)!=1 || !(player2_char >= 0 && player2_char <= 10 )){
+		if(BotOn==1){
+			printf("請選擇電腦要遊玩的角色：");
+		}else{
+			printf("玩家二請選擇你要遊玩的角色：");
+		}
+		
+	} 
+	
+	
+	
+	if(mode == 2 ){
+		printf("====================\n");
+		printf("0)小紅帽\n");
+		printf("1)白雪公主\n");
+		printf("2)睡美人\n");
+		printf("3)愛麗絲\n");
+		printf("4)花木蘭\n");
+		printf("5)輝夜姬\n");
+		printf("6)美人魚\n");
+		printf("7)火柴女孩\n");
+		printf("8)逃樂絲\n");
+		printf("9)山魯佐德\n");
+		printf("10)隨機\n");
+		printf("====================\n");
+		printf("玩家三請選擇你要遊玩的角色：");
+		while (scanf("%hhd",&player3_char)!=1 || !(player3_char >= 0 && player3_char <= 10 )||(player3_char == player1_char)||(player3_char == player2_char)){
 			getchar();
-			printf("請你重新輸入\n");
+			printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 			printf("====================\n");
 			printf("0)小紅帽\n");
 			printf("1)白雪公主\n");
 			printf("2)睡美人\n");
+			printf("3)愛麗絲\n");
+			printf("4)花木蘭\n");
+			printf("5)輝夜姬\n");
+			printf("6)美人魚\n");
+			printf("7)火柴女孩\n");
+			printf("8)逃樂絲\n");
+			printf("9)山魯佐德\n");
 			printf("10)隨機\n");
 			printf("====================\n");
-			printf("玩家二請選擇你要遊玩的角色：");
+			printf("玩家三請選擇你要遊玩的角色：");
+		} 
+		
+		
+		printf("\n====================\n");
+		printf("0)小紅帽\n");
+		printf("1)白雪公主\n");
+		printf("2)睡美人\n");
+		printf("3)愛麗絲\n");
+		printf("4)花木蘭\n");
+		printf("5)輝夜姬\n");
+		printf("6)美人魚\n");
+		printf("7)火柴女孩\n");
+		printf("8)逃樂絲\n");
+		printf("9)山魯佐德\n");
+		printf("10)隨機\n");
+		printf("====================\n");
+		if(BotOn==1){
+			printf("請選擇電腦二要遊玩的角色：");
+		}else{
+			printf("玩家四請選擇你要遊玩的角色：");
+		}
+		while (scanf("%hhd",&player4_char)!=1 || !(player4_char >= 0 && player4_char <= 10 )||(player4_char == player1_char)||(player4_char == player2_char)||(player4_char == player3_char)){
+			getchar();
+			printf("輸入了不可選的角色或是其他人已選擇的角色\n");
+			printf("====================\n");
+			printf("0)小紅帽\n");
+			printf("1)白雪公主\n");
+			printf("2)睡美人\n");
+			printf("3)愛麗絲\n");
+			printf("4)花木蘭\n");
+			printf("5)輝夜姬\n");
+			printf("6)美人魚\n");
+			printf("7)火柴女孩\n");
+			printf("8)逃樂絲\n");
+			printf("9)山魯佐德\n");
+			printf("10)隨機\n");
+			printf("====================\n");
+			if(BotOn==1){
+				printf("請選擇電腦二要遊玩的角色：");
+			}else{
+				printf("玩家四請選擇你要遊玩的角色：");
+			}
+			
 		} 
 	}
+	
+	
 	
 	
 	
