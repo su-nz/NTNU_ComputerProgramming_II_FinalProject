96667465ea3c2e1b3c66879e0a32f2b16b27773c
LeoTengAMao <LeoTeng995@gmail.com>
2025-04-22 15:25:14 +0800

    Update TwistedFables.h

--- TwistedFables.h
+++ TwistedFables.h
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <time.h> 
 #include <unistd.h> 
+#include "vector.h"
 #define Ult_threshold Ultt
 #define Maxpower 25
 #define DeckMax 200
@@ -17,6 +18,21 @@ typedef struct Character{
 	int8_t character_code; // 角色代碼
 	int8_t token_code; // 偷肯代碼 0)為沒有
 }character;
+
+typedef struct Card{
+	int8_t cost;
+	int8_t level;
+	int8_t range; //for attack 0 means its doesn't need range
+	int8_t type; // 0)attack 1)defend 2)movement 3)skills 4)Ult 5)passive
+	int8_t power_generate;
+	int8_t value; // for example value = 1 on attack => deal 1 damage
+	int8_t damage;
+	int8_t shield;
+	int8_t require_basic_card; // 0 means do not requires
+	int16_t cardcode; // for skill card 
+	char * inf;
+	char * cardname;
+}card;
 
 typedef struct Player{
 	int8_t character; // 0 - 9
@@ -40,23 +56,18 @@ typedef struct Player{
 	int8_t combo; //桃樂絲 連擊 -1代表不是桃樂絲
 	int8_t tentacle;
 	int8_t Scheherazade_token;
+	vector discard;
+	vector deck;
+	card hands_card[50];
 	
 }player;
 
-typedef struct Card{
-	int8_t cost;
-	int8_t level;
-	int8_t range; //for attack 0 means its doesn't need range
-	int8_t type; // 0)attack 1)defend 2)movement 3)skills 4)Ult 5)passive
-	int8_t power_generate;
-	int8_t value; // for example value = 1 on attack => deal 1 damage
-	int8_t damage;
-	int8_t shield;
-	int8_t require_basic_card; // 0 means do not requires
-	int16_t cardcode; // for skill card 
-	char * inf;
-	char * cardname;
-}card;
+
+
+typedef struct Deck{
+	card C[256];
+	int16_t size;
+}deck;
 
 
 
