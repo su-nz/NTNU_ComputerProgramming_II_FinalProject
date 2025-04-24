1d0f4ca306e829e6196be58eba59906157bf225a
LeoTengAMao <LeoTeng995@gmail.com>
2025-04-25 00:35:39 +0800

    Update TwistedFables.c

--- TwistedFables.c
+++ TwistedFables.c
@@ -9,6 +9,7 @@ vector basicBuyDeck[4][3];  // attack(0) LV1~3 defense(1) LV1~3 move(2) LV1~3 ge
 vector skillBuyDeck[4][3];  // 4 player 
 
 
+
 player player_1,player_2,player_3,player_4;
 int8_t mode=-1;
 int8_t RelicOn = -1;
@@ -17,7 +18,7 @@ int8_t player1_char = -1;
 int8_t player2_char = -1;
 int8_t player3_char = -1;
 int8_t player4_char = -1;
-
+int8_t turn = 0;
 int8_t print_hands(player *P){
 	for(int i = 0 ; i < P->hands ; i++){
 		printf("%d) %s 效果：%s\n",i+1,P->hands_card[i].cardname,P->hands_card[i].inf);
@@ -76,6 +77,7 @@ int8_t initialization_skill_shop(player *P){
 	
 	switch (P->character){
 		case 0:
+		{
 			int8_t temp1[] = {138,13,13,13,135,12,12,11};
 			for (int i = 0; i < 8; ++i) {
 			    skillBuyDeck[P->num][0].array[i] = temp1[i];
@@ -87,12 +89,12 @@ int8_t initialization_skill_shop(player *P){
 			int8_t temp3[] = {138,19,19,19,137,18,18,17};
 			for (int i = 0; i < 8; ++i) {
 			    skillBuyDeck[P->num][2].array[i]  = temp3[i];
-			}
-			
-			
-		break;
+			}	
+			break;
+		}
 		
 		case 1:
+		{
 			int temp4[] = {142,25,25,25,139,24,24,23};
 			for (int i = 0; i < 8; ++i) {
 			    skillBuyDeck[P->num][0].array[i] = temp4[i];
@@ -107,14 +109,11 @@ int8_t initialization_skill_shop(player *P){
 			for (int i = 0; i < 8; ++i) {
 			    skillBuyDeck[P->num][2].array[i] = temp6[i];
 			}
-					
-		break;
+			break;
+		}
 	}
 	return 0; // 成功
 }
-
-
-
 
 int8_t printf_basic_shop(){
 	system("clear");
@@ -160,7 +159,11 @@ int8_t skill_shop_command(player *P){
 	int8_t ssc =-1;
 	printf_skill_shop(P->num);
 	while(1){
-		printf("請輸入你要做什麼？1.)升級技能 2.)退出商店：");
+		printf("請輸入你要做什麼？\n");
+		printf("1. 升級技能\n");
+		printf("2. 退出商店\n");
+		printf("> ");
+
 		scanf("%hhd",&ssc);
 		getchar();
 		if(ssc == 1){
@@ -248,8 +251,10 @@ int8_t discard_command(player *P){
 	int8_t dc =-1;
 	print_discard(P);
 	while(1){
-		printf("請輸入你要做什麼？1.)查看 2.)返回\n");
-		printf("請輸入：");
+		printf("請輸入你要做什麼？\n");
+		printf("1. 查看\n");
+		printf("2. 返回\n");
+		printf("> ");
 		scanf("%hhd",&dc);
 		getchar();
 		if(dc == 1){
@@ -279,9 +284,11 @@ int8_t discard_command(player *P){
 int8_t basic_shop_command(player *P){
 	int8_t bsc =-1;
 	printf_basic_shop();
-	while(1){
-		
-		printf("請輸入你要做什麼？1.)購買卡片 2.)退出商店：");
+	while(1){	
+		printf("請輸入你要做什麼？\n");
+		printf("1. 購買卡片\n");
+		printf("2. 退出商店\n");
+		printf("> ");
 		scanf("%hhd",&bsc);
 		getchar();
 		if(bsc == 1){
@@ -483,8 +490,11 @@ int8_t basic_shop_command(player *P){
 int8_t remove_card(player *P){
 	system("clear");
 	int8_t comm=-1;
-	printf("請問你要對哪一個地方使用專注？ 0.）手牌 1.）棄牌堆 2.）返回\n");
-	printf("輸入指令：");
+	printf("請問你要對哪一個地方使用專注？\n");
+	printf("0. 手牌\n");
+	printf("1. 棄牌堆\n");
+	printf("2. 返回\n");
+	printf("> ");
 	scanf("%hhd",&comm);
 	switch(comm){
 		case 0:
@@ -565,24 +575,71 @@ int8_t remove_card(player *P){
 	}
 }
 
-
+int8_t play_a_card(player *P){
+	system("clear");
+	print_game_broad_9();
+	print_hands(P);
+	printf("你要打哪一張牌？（輸入0返回）\n");
+	while(1){
+		int8_t select=-1;
+		if((*P).hands == 0){
+			if(mode == 1){
+				print_game_broad_9();
+						
+				}else{			
+			}
+			printf("你沒有手牌了！\n");
+			action_command(P);
+		}
+		printf("請問你要打哪一張牌？\n");
+		printf("輸入數字：");
+		scanf("%hhd",&select);
+		if(select == 0){
+			if(mode == 1){
+				print_game_broad_9();
+				return 0;
+			}else{
+				
+			}
+			action_command(P);
+			return 0;
+		}
+	
+		if(select > P->hands || select < 0 ){
+				printf("沒有這張卡！\n");
+		}else{
+			switch (P->hands_card[select-1].type){
+				case 0:
+					
+				break;
+					
+			}
+			return -1;
+		}
+				
+	}
+}
 
 int8_t action_command(player *P){
 	int8_t comm=-1;
 	
-	printf("現在是%s的回合，現在是你的執行階段請從以下的動作中選一個執行\n",(*P).charname);
-	printf("0.)專注 1.)購買基礎牌 2.)購買技能牌 3.)打牌 4.)查看自己的棄牌堆 5.)查看別人的棄牌堆 6.)結束回合\n");
+	printf("現在是"GREEN BOLD"%s"RESET"的回合，現在是你的執行階段請從以下的動作中選一個執行\n",(*P).charname);
+	printf("1. 購買基礎牌\n");
+	printf("2. 購買技能牌\n");
+	printf("3. 打牌\n");
+	printf("4. 查看自己的棄牌堆\n");
+	printf("5. 查看別人的棄牌堆\n");
+	printf("6. 結束回合\n\n");
+
 	printf("你的手牌：\n");
 	print_hands(P);
 	printf("輸入指令：");
 	scanf("%hhd",&comm);
 	getchar();
 	switch(comm){
-		case 0:  // 專注
-			remove_card(P) ;
-			return -1;
 		
-            	break;
+		
+            	
             	
             	case 1:  // 購買基礎牌
 			basic_shop_command(P);
@@ -595,17 +652,21 @@ int8_t action_command(player *P){
             	break;
             	
             	case 3:  // 打牌
-			
+			play_a_card(P);
 		
             	break;
             	
             	case 4:  // 查看自己的棄牌
-			discard_command(&player_1);
+            		
+			discard_command(P);
+			
 		
             	break;
             	
             	case 5:  // 查看別人的棄牌堆
-			discard_command(&player_2);
+            		if(mode ==1){// 新增不同回合要換人
+				discard_command(&player_2);
+			}
 		
             	break;
             	
@@ -659,73 +720,219 @@ void print_extra_inf(player *P){
 	}
 }
 
-void print_game_broad_9(){
-	system("clear");
-	
-		printf("玩家一 │ 遊玩角色：%s 血量：%hhd 防禦值：%hhd 能量：%hhd 手牌數：%hhd 血量上限：%hhd 防禦上限：%hhd 必殺閥值:%hhd\n",player_1.charname,player_1.hp,player_1.armor,player_1.power,player_1.hands,player_1.Maxhp,player_1.Maxarmor,player_1.Ult_threshold);
-		print_extra_inf(&player_1);
-		printf("玩家二 │ 遊玩角色：%s 血量：%hhd 防禦值：%hhd 能量：%hhd 手牌數：%hhd 血量上限：%hhd 防禦上限：%hhd 必殺閥值:%hhd\n",player_2.charname,player_2.hp,player_2.armor,player_2.power,player_2.hands,player_2.Maxhp,player_2.Maxarmor,player_2.Ult_threshold);
-		print_extra_inf(&player_2);
-		printf("          ┌───────────────────┐           \n");
-		
-		//printf("     Board│ O O O O O O O O O │版面                   \n");//10
-		printf("     Board│ ");
-		for(int i = 0 ; i < 9 ; i++){
-			if(player_1.coordinate == i){
-				printf("1 ");
-			}else if(player_2.coordinate == i){
-				printf("2 ");
-			}else{
-				printf("_ ");
-			}	
-		}
-		printf("│版面                   \n");
-		printf("          ├───────────────────┤           \n");
-		printf("Coordinate│ 0 1 2 3 4 5 6 7 8 │座標                   \n");
-		printf("          └───────────────────┘           \n");
-	
-		return ;
+int8_t utf8_strlen(const char *s) {
+	int8_t len = 0;
+	while (*s) {
+		if ((*s & 0x80) == 0) s += 1;         // 英文
+		else if ((*s & 0xE0) == 0xC0) s += 2; // 少見
+		else if ((*s & 0xF0) == 0xE0) s += 3; // 中文
+		else if ((*s & 0xF8) == 0xF0) s += 4; // emoji
+		else s += 1;
+		len++;
+	}
+	return len;
 }
 
+void print_game_broad_9(){
+	int8_t len = 0;
+	if(utf8_strlen(player_1.charname) == 3 && utf8_strlen(player_2.charname) == 3) len = 6;
+	else len = 8;
+	system("clear");
+	printf("玩家一 │ 遊玩角色：");
+	print_aligned_charname(player_1.charname, len);
+	printf(" 血量：%hhd 防禦值：%hhd 能量：%hhd 手牌數：%hhd 血量上限：%hhd 防禦上限：%hhd 必殺閥值:%hhd\n", player_1.hp, player_1.armor, player_1.power, player_1.hands, player_1.Maxhp, player_1.Maxarmor, player_1.Ult_threshold);
+	print_extra_inf(&player_1);
 
+	printf("玩家二 │ 遊玩角色：");
+	print_aligned_charname(player_2.charname, len);
+	printf(" 血量：%hhd 防禦值：%hhd 能量：%hhd 手牌數：%hhd 血量上限：%hhd 防禦上限：%hhd 必殺閥值:%hhd\n", player_2.hp, player_2.armor, player_2.power, player_2.hands, player_2.Maxhp, player_2.Maxarmor, player_2.Ult_threshold);
+	print_extra_inf(&player_2);
 
+	printf("          ┌───────────────────┐           \n");
+	
+	//printf("     Board│ O O O O O O O O O │版面                   \n");//10
+	printf("     Board│ ");
+	for(int i = 0 ; i < 9 ; i++){
+		if(player_1.coordinate == i){
+			printf("1 ");
+		}else if(player_2.coordinate == i){
+			printf("2 ");
+		}else{
+			printf("_ ");
+		}	
+	}
+	printf("│版面                   \n");
+	printf("          ├───────────────────┤           \n");
+	printf("Coordinate│ 1 2 3 4 5 6 7 8 9 │座標                   \n");
+	printf("          └───────────────────┘           \n");
+	return ;
+}
+
+void print_header()
+{
+	printf("\033[2J");
+	printf("\033[H");
+	printf("╔═══════════════════════╗\n");
+	printf("║     Twisted Fables    ║\n");
+	printf("║    反轉寓言：文字版   ║\n");
+	printf("╚═══════════════════════╝\n");
+
+	printf("遊戲模式　：");
+	if(mode == 1) printf("1對1\n");
+	else if(mode == 2) printf("2對2\n");
+	else printf("（未選擇）\n");
+
+	printf("遺跡模式　：");        
+	if(RelicOn == 1) printf("是\n");        
+	else if(RelicOn == 2) printf("否\n");        
+	else printf("（未選擇）\n");
+
+	printf("啟用機器人：");        
+	if(BotOn == 1) printf("是\n");        
+	else if(BotOn == 2) printf("否\n");        
+	else printf("（未選擇）\n");
+
+	printf("玩家一　　：");	
+	if(player_1.charname) printf("%s\n", player_1.charname);	
+	else printf("（未選擇）\n");
+	
+	printf("玩家二　　：");        
+	if(player_2.charname) printf("%s\n", player_2.charname);        
+	else printf("（未選擇）\n");
+
+	printf("玩家三　　：");        
+	if(player_3.charname) printf("%s\n", player_3.charname);        
+	else printf("（未選擇）\n");
+
+	printf("玩家四　　：");        
+	if(player_4.charname) printf("%s\n\n", player_4.charname);        
+	else printf("（未選擇）\n\n");
+}
+
+int8_t get_random_unused_character(int8_t remaining_roles[], int8_t remaining_count) {
+	if (remaining_count <= 0) return -1;
+
+	int8_t pick = rand() % remaining_count;
+	for (int8_t i = 0; i < 10; i++) {        
+		if (remaining_roles[i]) {            
+			if (pick == 0) return i;            
+			pick--;        
+		}    
+	}
+	return -1;
+}
+
+void enable_raw_mode() {
+	struct termios t;
+	tcgetattr(STDIN_FILENO, &t);
+	t.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+
+void disable_raw_mode() {
+	struct termios t;
+	tcgetattr(STDIN_FILENO, &t);
+	t.c_lflag |= ICANON | ECHO;
+	tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
+
+void wait_for_space() {
+	enable_raw_mode();
+	printf("按下空白鍵開始遊戲");
+	int8_t ch;
+	while ((ch = getchar()) != ' ') {}
+	disable_raw_mode();
+}
+
+int8_t str_display_width(const char *s) {
+	int8_t width = 0;
+    	while (*s) {
+		if ((*s & 0x80) == 0) {
+			width += 1;
+			s++;
+		} else {
+			width += 2;
+			s += 3;
+		}
+	}
+	return width;
+}
+
+void print_aligned_charname(const char* name, int8_t width) {    
+	int8_t actual_width = str_display_width(name);    
+	printf("%s", name);    
+	for (int8_t i = 0; i < width - actual_width; i++) putchar(' ');
+}
 
 int main(){ //mainfuc
-
+	system("clear");
+	print_header();
 	srand( time(NULL) );
 	
 	initialization_basic_shop();
+	
 	//mode=-1
-	printf("請你選擇你想要遊玩的模式：(1.)1對1 (2.)2對2 （請輸入數字來做決定\n");
+	printf("請選擇遊戲模式：\n");
+	printf("1. 1對1\n");
+	printf("2. 2對2\n");
+	printf("> ");
 	while (scanf("%hhd",&mode)!=1 || !(mode == 1 || mode ==2 )){
 		getchar();
+		print_header();
 		printf("請你重新輸入\n");
-		printf("請你選擇你想要遊玩的模式：(1.)1對1 (2.)2對2 （請輸入數字來做決定\n");
-		
+		printf("請選擇遊戲模式：\n");
+	        printf("1. 1對1\n");
+        	printf("2. 2對2\n");
+		printf("> ");
 	} 
 	player_1.num = 0;
 	player_2.num = 1;
 	player_3.num = 2;
 	player_4.num = 3;
 	
-	player_1.coordinate = 3;
-	player_2.coordinate = 5;
+	player_1.coordinate = 4;
+	player_2.coordinate = 6;
+	
 	//RelicOn = -1
-	printf("是否要啟動遺跡模式？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+	print_header();
+	printf("是否要啟動遺跡模式：\n");
+	printf("1. 是\n");
+	printf("2. 否\n");
+	printf("> ");
 	while (scanf("%hhd",&RelicOn)!=1 || !(RelicOn == 1 || RelicOn == 2 )){
 		getchar();
+		print_header();
 		printf("請你重新輸入\n");
-		printf("是否要啟動遺跡模式？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+		printf("是否要啟動遺跡模式：\n");
+	        printf("1. 是\n");
+       		printf("2. 否\n");
+	        printf("> ");
 	} 
+	
 	//BotOn = -1
-	printf("是否要啟用機器人？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+	print_header();
+	printf("是否要啟用機器人：\n");
+	printf("1. 是\n");
+        printf("2. 否\n");
+        printf("> ");
 	while (scanf("%hhd",&BotOn)!=1 || !(BotOn == 1 || BotOn == 2 )){
 		getchar();
+		print_header();
 		printf("請你重新輸入\n");
-		printf("是否要啟用機器人？：(1.)是 (2.)否 （請輸入數字來做決定\n");
+		printf("是否要啟用機器人：\n");
+	        printf("1. 是\n");
+        	printf("2. 否\n");
+	        printf("> ");
 	} 
+	
 	//player1_char = -1
-	printf("\n====================\n");
+	int8_t remaining_roles[10];
+	for (int8_t i = 0; i < 10; i++) {
+		remaining_roles[i] = 1;
+	}
+	int8_t remaining_count = 10;
+	print_header();
+	printf("====================\n");
 	printf("0)小紅帽\n");
 	printf("1)白雪公主\n");
 	printf("2)睡美人\n");
@@ -741,6 +948,7 @@ int main(){ //mainfuc
 	printf("玩家一請選擇你要遊玩的角色：");
 	while (scanf("%hhd",&player1_char)!=1 || !(player1_char >= 0 && player1_char <= 10 )){
 		getchar();
+		print_header();
 		printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 		printf("====================\n");
 		printf("0)小紅帽\n");
@@ -759,11 +967,14 @@ int main(){ //mainfuc
 		
 	} 
 	if(player1_char == 10){
-		player1_char = (rand()%9+1); 
+		player1_char = get_random_unused_character(remaining_roles, remaining_count);
 	}
 	inputcharacter(&player_1, player1_char);
+	remaining_roles[player1_char] = 0;
+	remaining_count--;
 	
-	printf("\n====================\n");
+	print_header();
+	printf("====================\n");
 	printf("0)小紅帽\n");
 	printf("1)白雪公主\n");
 	printf("2)睡美人\n");
@@ -783,6 +994,7 @@ int main(){ //mainfuc
 	}
 	while (scanf("%hhd",&player2_char)!=1 || !(player2_char >= 0 && player2_char <= 10 )||(player2_char == player1_char)){
 		getchar();
+		print_header();
 		printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 		printf("====================\n");
 		printf("0)小紅帽\n");
@@ -805,11 +1017,14 @@ int main(){ //mainfuc
 		
 	} 
 	if(player2_char == 10){
-		player2_char = (rand()%9+1); 
-	}
-	inputcharacter(&player_2, player2_char);
+                player2_char = get_random_unused_character(remaining_roles, remaining_count);
+        }
+        inputcharacter(&player_2, player2_char);
+        remaining_roles[player2_char] = 0;
+        remaining_count--;
 	
 	if(mode == 2 ){
+		print_header();
 		printf("====================\n");
 		printf("0)小紅帽\n");
 		printf("1)白雪公主\n");
@@ -826,6 +1041,7 @@ int main(){ //mainfuc
 		printf("玩家三請選擇你要遊玩的角色：");
 		while (scanf("%hhd",&player3_char)!=1 || !(player3_char >= 0 && player3_char <= 10 )||(player3_char == player1_char)||(player3_char == player2_char)){
 			getchar();
+			print_header();
 			printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 			printf("====================\n");
 			printf("0)小紅帽\n");
@@ -843,10 +1059,14 @@ int main(){ //mainfuc
 			printf("玩家三請選擇你要遊玩的角色：");
 		} 
 		if(player3_char == 10){
-			player3_char = (rand()%9+1); 
-		}
-		inputcharacter(&player_3, player3_char);
-		printf("\n====================\n");
+	                player3_char = get_random_unused_character(remaining_roles, remaining_count);
+	        }
+	        inputcharacter(&player_3, player3_char);
+	        remaining_roles[player3_char] = 0;
+	        remaining_count--;
+		
+		print_header();
+		printf("====================\n");
 		printf("0)小紅帽\n");
 		printf("1)白雪公主\n");
 		printf("2)睡美人\n");
@@ -866,6 +1086,7 @@ int main(){ //mainfuc
 		}
 		while (scanf("%hhd",&player4_char)!=1 || !(player4_char >= 0 && player4_char <= 10 )||(player4_char == player1_char)||(player4_char == player2_char)||(player4_char == player3_char)){
 			getchar();
+			print_header();
 			printf("輸入了不可選的角色或是其他人已選擇的角色\n");
 			printf("====================\n");
 			printf("0)小紅帽\n");
@@ -888,16 +1109,20 @@ int main(){ //mainfuc
 			
 		} 
 		if(player4_char == 10){
-			player4_char = (rand()%9+1); 
-		}
-		inputcharacter(&player_4, player4_char);
+	                player4_char = get_random_unused_character(remaining_roles, remaining_count);
+	        }
+	        inputcharacter(&player_4, player4_char);
+	        remaining_roles[player4_char] = 0;
+	        remaining_count--;
 	}
 	
 	initialization_skill_shop(&player_1);
 	initialization_skill_shop(&player_2);
 	initialization_deck(&player_1);
-	initialization_deck(&player_2);
-	
+	initialization_deck(&player_2);	
+
+	print_header();	
+	wait_for_space();
 	//遊戲開始
 	int8_t round = 0;
 	int8_t flip_coins=0;
@@ -921,6 +1146,7 @@ int main(){ //mainfuc
 		if(mode == 1){//單人模式
 			round++;
 			while(1){ // 執行階段
+				 turn = 0;
 				if(player_1.first == 1){//玩家一先手
 					if(round == 1){
 						draw_card(4,&player_1);
@@ -928,6 +1154,7 @@ int main(){ //mainfuc
 						draw_card(6,&player_1);
 					}
 					draw_card(6,&player_2);
+					//補一個專注
 					print_game_broad_9();
 					if(round % 2 == 1){
 						if(round == 1) printf("玩家一先手\n");	
@@ -957,11 +1184,6 @@ int main(){ //mainfuc
 		
 		
 		
-	}
-	
-	
-	
-	
-  
+	} 
 	return 0;
 }
