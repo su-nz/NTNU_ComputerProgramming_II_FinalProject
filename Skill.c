1006a811cb6aff86309fb8159148dae5cab6de35
LeoTengAMao <LeoTeng995@gmail.com>
2025-06-09 15:08:50 +0800

    Update Skill.c

--- Skill.c
+++ Skill.c
@@ -48,7 +48,7 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 	}
 	
 	if(card_id == 20){
-		
+		system("clear");
 		
 		card cardtemp1;
 		Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
@@ -71,25 +71,37 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 							printf("此卡已經賣光了\n") ;
 							
 						}else{
-							pushbackVector(&P->discard, skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1]);
-							skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1] = 0;
-							skillBuyDeck[P->num][0].SIZE--;
+							pushbackVector(&you->discard, skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1]);
+							skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1] = 0;
+							skillBuyDeck[you->num][0].SIZE--;
 							if(skillBuyDeck[you->num][0].SIZE != 0){
-									Card_Define(skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1], &cardtemp1);
+									Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
 								
 								if(cardtemp1.cost == 0){
-									P->passive[P->passive_n] = skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1];
-									skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1] = 0;
-									skillBuyDeck[P->num][0].SIZE--;
-									P->passive_n++;
-									Card_Define(skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1], &cardtemp1);
+									you->passive[you->passive_n] = skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1] = 0;
+									skillBuyDeck[you->num][0].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
 								}
 							}
 							if(skillBuyDeck[you->num][0].SIZE != 0){
-								Card_Define(skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1], &cardtemp1);
-								pushbackVector(&P->discard, skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1]);
-								skillBuyDeck[P->num][0].array[skillBuyDeck[P->num][0].SIZE-1] = 0;
-								skillBuyDeck[P->num][0].SIZE--;
+								Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
+								pushbackVector(&you->discard, skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1]);
+								skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1] = 0;
+								skillBuyDeck[you->num][0].SIZE--;
+							}
+							
+							if(skillBuyDeck[you->num][0].SIZE != 0){
+									Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
+								
+								if(cardtemp1.cost == 0){
+									you->passive[you->passive_n] = skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1] = 0;
+									skillBuyDeck[you->num][0].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][0].array[skillBuyDeck[you->num][0].SIZE-1], &cardtemp1);
+								}
 							}
 						}
 						
@@ -100,24 +112,35 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 							printf("此卡已經賣光了\n") ;
 							
 						}else{
-							pushbackVector(&P->discard, skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1]);
-							skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1] = 0;
-							skillBuyDeck[P->num][1].SIZE--;
+							pushbackVector(&you->discard, skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1]);
+							skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1] = 0;
+							skillBuyDeck[you->num][1].SIZE--;
 							if(skillBuyDeck[you->num][1].SIZE != 0){
-								Card_Define(skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1], &cardtemp2);
+								Card_Define(skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1], &cardtemp2);
 								if(cardtemp2.cost == 0){
-									P->passive[P->passive_n] = skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][0].SIZE-1];
-									skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1] = 0;
-									skillBuyDeck[P->num][1].SIZE--;
-									P->passive_n++;
-									Card_Define(skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1], &cardtemp2);
+									you->passive[you->passive_n] = skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1] = 0;
+									skillBuyDeck[you->num][1].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1], &cardtemp2);
 								}
 							}
 							if(skillBuyDeck[you->num][1].SIZE != 0){
-								Card_Define(skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1], &cardtemp2);
-								pushbackVector(&P->discard, skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1]);
-								skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][1].SIZE-1] = 0;
-								skillBuyDeck[P->num][1].SIZE--;
+								Card_Define(skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1], &cardtemp2);
+								pushbackVector(&you->discard, skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1]);
+								skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1] = 0;
+								skillBuyDeck[you->num][1].SIZE--;
+							}
+							
+							if(skillBuyDeck[you->num][1].SIZE != 0){
+								Card_Define(skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1], &cardtemp2);
+								if(cardtemp2.cost == 0){
+									you->passive[you->passive_n] = skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1] = 0;
+									skillBuyDeck[you->num][1].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][1].SIZE-1], &cardtemp2);
+								}
 							}
 						}
 						
@@ -128,24 +151,35 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 							printf("此卡已經賣光了\n") ;
 							
 						}else{
-							pushbackVector(&P->discard, skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1]);
-							skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1] = 0;
-							skillBuyDeck[P->num][2].SIZE--;
+							pushbackVector(&you->discard, skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1]);
+							skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1] = 0;
+							skillBuyDeck[you->num][2].SIZE--;
 							if(skillBuyDeck[you->num][2].SIZE != 0){
-								Card_Define(skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1], &cardtemp3);
+								Card_Define(skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1], &cardtemp3);
 								if(cardtemp3.cost == 0){
-									P->passive[P->passive_n] = skillBuyDeck[P->num][1].array[skillBuyDeck[P->num][0].SIZE-1];
-									skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1] = 0;
-									skillBuyDeck[P->num][2].SIZE--;
-									P->passive_n++;
-									Card_Define(skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1], &cardtemp2);
+									you->passive[you->passive_n] = skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1] = 0;
+									skillBuyDeck[you->num][2].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1], &cardtemp2);
 								}
 							}
 							if(skillBuyDeck[you->num][2].SIZE != 0){
-								Card_Define(skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1], &cardtemp3);
-								pushbackVector(&P->discard, skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1]);
-								skillBuyDeck[P->num][2].array[skillBuyDeck[P->num][2].SIZE-1] = 0;
-								skillBuyDeck[P->num][2].SIZE--;
+								Card_Define(skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1], &cardtemp3);
+								pushbackVector(&you->discard, skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1]);
+								skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1] = 0;
+								skillBuyDeck[you->num][2].SIZE--;
+							}
+							
+							if(skillBuyDeck[you->num][2].SIZE != 0){
+								Card_Define(skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1], &cardtemp3);
+								if(cardtemp3.cost == 0){
+									you->passive[you->passive_n] = skillBuyDeck[you->num][1].array[skillBuyDeck[you->num][0].SIZE-1];
+									skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1] = 0;
+									skillBuyDeck[you->num][2].SIZE--;
+									you->passive_n++;
+									Card_Define(skillBuyDeck[you->num][2].array[skillBuyDeck[you->num][2].SIZE-1], &cardtemp2);
+								}
 							}
 						}
 						
@@ -156,7 +190,6 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 						printf("不存在此選項\n");
 					break;
 					
-					
 				}
 				
 			
@@ -166,6 +199,46 @@ int8_t use_Ult(player* you,player *P,int16_t card_id , int8_t *damage_output , i
 	}else if(card_id == 21){
 	
 	}else if(card_id == 22){
+		int times=0;
+		while(1){
+			printf("\n請輸入你要擊退的距離(0-3)\n>");
+			scanf("%d",&times);
+			getchar();
+			if(times >= 0  && times <= 3){
+				break;
+			}else{
+				printf("你的範圍錯誤\n");
+			}
+		}
+		*damage_output = 3;
+		for(int i = 0 ; i < times ; i++){
+			if(P->coordinate + way <= Right_MAX  && P->coordinate + way > 0){
+				P->coordinate += way;
+			}
+		}
+		
+		int left = 3;
+		for(int i = 0 ; i < 3 ; i++){
+			if(P->hands == 0) break;
+			
+			while(1){
+				int8_t select=0;
+				system("clear");
+				printf("%s請你棄置3張牌，還剩下%d張牌要棄置\n",P->charname,left);
+				print_hands(P);
+				printf("請輸入你要棄置的牌\n>");
+				printf("輸入數字：");
+				scanf("%hhd",&select);
+				if(select > P->hands || select <= 0 ){
+					printf("沒有這張卡！\n");
+				}else{
+					discard_card_from_hand(P,select-1);
+					break;
+				}
+			}
+			left--;
+			
+		}
 		
 	}
 }
@@ -210,24 +283,57 @@ int8_t use_skill(player* you,player *P,int16_t card_id , int8_t *damage_output ,
 		*armor_output = lv;
 		add_card_to_starting(you,16,combo_cardid,lv);
 	}else if(card_id == 17){
+		int times=0;
+		while(1){
+			printf("\n請輸入你要擊退的距離(0-%d)\n>",lv);
+			scanf("%d",&times);
+			getchar();
+			if(times >= 0  && times <= lv){
+				break;
+			}else{
+				printf("你的範圍錯誤\n");
+			}
+		}
 		*damage_output = 1;
-		for(int i = 0 ; i < lv ; i++){
-			if(P->coordinate + way <= Right_MAX && P->coordinate + way > 0){
+		for(int i = 0 ; i < times ; i++){
+			if(P->coordinate + way <= Right_MAX  && P->coordinate + way > 0){
 				P->coordinate += way;
 			}
 		}
 		
 	}else if(card_id == 18){
+		int times=0;
+		while(1){
+			printf("\n請輸入你要擊退的距離(0-%d)\n>",lv);
+			scanf("%d",&times);
+			getchar();
+			if(times >= 0  && times <= lv){
+				break;
+			}else{
+				printf("你的範圍錯誤\n");
+			}
+		}
 		*damage_output = 2;
-		for(int i = 0 ; i < lv ; i++){
+		for(int i = 0 ; i < times ; i++){
 			if(P->coordinate + way <= Right_MAX  && P->coordinate + way > 0){
 				P->coordinate += way;
 			}
 		}
 		
 	}else if(card_id == 19){
+		int times=0;
+		while(1){
+			printf("\n請輸入你要擊退的距離(0-%d)\n>",lv);
+			scanf("%d",&times);
+			getchar();
+			if(times >= 0  && times <= lv){
+				break;
+			}else{
+				printf("你的範圍錯誤\n");
+			}
+		}
 		*damage_output = 3;
-		for(int i = 0 ; i < lv ; i++){
+		for(int i = 0 ; i < times ; i++){
 			if(P->coordinate + way <= Right_MAX  && P->coordinate + way > 0){
 				P->coordinate += way;
 			}
