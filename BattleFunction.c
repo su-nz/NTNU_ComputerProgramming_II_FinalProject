7bfb2bd9e0eae98b346faa5d12c905135b3f5173
LeoTengAMao <LeoTeng995@gmail.com>
2025-06-15 20:33:20 +0800

    Update BattleFunction.c

--- BattleFunction.c
+++ BattleFunction.c
@@ -1,25 +1,3 @@
-#include "TwistedFables.h"
 
-int discard_back_to_deck(player *P){
-	for(int i = 0 ; i < P->discard.size ; i++){
-		pushbackVector(vector* vec, int32_t val)
-			
-		
-	}
-}
-
-int8_t draw_card(int8_t amount , player *P){
-	for(int i = 0 ; i < amount ; i++){
-		if(P->deck.SIZE !=0){
-			Card_Define(P->deck.array[P->deck.SIZE-1] , P->hands_card[hands]);
-			P->deck.array[P->deck.SIZE-1] = 0; //remove card
-			P->deck.SIZE--;
-			P->hands++;
-		}else{
-			
-		}
-	}
-	return 0;
-}
 
 
